318b76b5cd9eb4d2155eb03e4eab0d5c6aca836e
amwtke <amwtke@gmail.com>
2025-05-15 10:35:17 +0800

    entiry

--- src/main/java/com/spring/ai/springaidemo/StructedOutput/StructuredOutputController.java
+++ src/main/java/com/spring/ai/springaidemo/StructedOutput/StructuredOutputController.java
@@ -2,26 +2,33 @@ package com.spring.ai.springaidemo.StructedOutput;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.ai.springaidemo.ChatClient.advisors.LogAdvisor;
 import com.spring.ai.springaidemo.StructedOutput.models.ActorFiles;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 public class StructuredOutputController {
 
-    private final ChatClient openAiChatClient;
+    private final ChatClient.Builder openAiChatClientBuilder;
+    private final LogAdvisor logAdvisor;
 
-    StructuredOutputController(ChatClient openAiChatClient) {
-        this.openAiChatClient = openAiChatClient;
+    StructuredOutputController(ChatClient.Builder openAiChatClientBuilder, LogAdvisor logAdvisor) {
+        this.openAiChatClientBuilder = openAiChatClientBuilder;
+        this.logAdvisor = logAdvisor;
     }
 
-    @GetMapping(value = "/ai/cc/actor_files", produces = "text/json;charset=utf-8")
-    public String genStructuredOutput(@RequestParam(value = "msg", defaultValue = "tom hanks") String msg) {
-        ActorFiles actor = openAiChatClient.prompt()
-                .user(u -> u.text("Generate the filmography of 5 movies for {actor}.").param("actor", msg))
+    @GetMapping(value = "/ai/cc/people_works", produces = "text/json;charset=utf-8")
+    public String genStructuredOutput(@RequestParam(value = "name", defaultValue = "tom hanks") String msg,
+                                      @RequestParam(value = "nr", defaultValue = "5") Integer nr) {
+        ActorFiles actor = openAiChatClientBuilder.build().prompt()
+                .user(u -> u.text("生成{people}的{nr}部作品结果。")
+                        .param("people", msg)
+                        .param("nr", nr)
+                )
+                .advisors(logAdvisor)
                 .call()
                 .entity(ActorFiles.class);
 
--- src/main/java/config/ChatClientConfig.java
+++ src/main/java/config/ChatClientConfig.java
@@ -10,12 +10,19 @@ import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.spring.ai.springaidemo.ChatClient.advisors.LogAdvisor.LOG_SWITCH;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
 @Configuration
 @Slf4j
 public class ChatClientConfig {
     @Bean
     ChatClient.Builder openAiChatClientBuilder(OpenAiChatModel openAiChatModel) {
-        return ChatClient.builder(openAiChatModel);
+        return ChatClient.builder(openAiChatModel)
+                .defaultAdvisors(a -> a.param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 4))
+                .defaultAdvisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, "default"))
+                .defaultAdvisors(a -> a.param(LOG_SWITCH, true));
     }
 
     @Bean
